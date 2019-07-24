875d5d1517512faf320f92b6fb52b7e9b96bbd3e
Nick O <nicholas.oetinger@gmail.com>
2019-07-24 18:30:37 -0400

    Created connection.js and orm.js files

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-const orm = require('../config/orm');
+const orm = require('../config/orm.js');
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 // Export at the end of
