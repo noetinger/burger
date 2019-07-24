90dc2d56589516f894e250bcd154d9a0f24e601e
Nick O <nicholas.oetinger@gmail.com>
2019-07-24 19:05:27 -0400

    initial draft of controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,51 @@
 const express = require('express');
+
+const router = express.Router();
+
+//Import model for database/ORM functions
 const burger = require('../models/burger.js');
 
-//create router for the app
+//Router for the app
+
+//get all data
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+//insert(create) new burger
+router.post("/api/burger", function (req, res) {
+    burger.insertOne([
+            burger_name
+        ],
+        [
+            req.body.name
+        ],
+        function (result) {
+            res.json({
+                id: result.insertId
+            });
+        });
+});
+
+
+//Update
+router.put("/api/burger/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: true
+    }, conditon, function (data) {
+        res.redirect("/");
+    });
+});
+
 //export the router
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,20 @@
+//Import connection for ORM
 const orm = require('../config/orm.js');
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+
 
 let burger = {
-    all: function(cb){
-        orm.selectAll("burger", function(res){
+    selectAll: function (cb) {
+        orm.selectAll("burger", function (res) {
             cb(res);
         })
     },
-    create: function(cols, vals, cb){
-        orm.insertOne("burer", cols, vals, function(res){
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burger", cols, vals, function (res) {
             cb(res);
         });
     },
-    update: function(objColVals, condition, cb){
-        orm.update("burger", objColVals, condition, function(res){
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burger", objColVals, condition, function (res) {
             cb(res);
         });
     }
